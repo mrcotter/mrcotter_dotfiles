d4da8443eb3235826b9e92288cbf2f84999b8cc0
MrCotter <mrcotter2013@gmail.com>
2015-06-26 19:46:30 +1000

    Add scripts for homebrew installation, other minor changes

---
+++ homebrew/brew_install.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+if test ! $(which brew); then
+	echo "Installing Homebrew"
+	ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
--- powerline-shell/install.sh
+++ powerline-shell/install.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Ask for the administrator password
 sudo -v
