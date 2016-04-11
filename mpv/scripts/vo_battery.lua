397a12226881858a15c76299867c498b992d6140
Kris Cotter <mrcotter2013@gmail.com>
2016-04-11 19:26:49 +1000

    Change default settings. Remove video-sync to reduce some lag problems.

--- mpv/scripts/vo_battery.lua
+++ mpv/scripts/vo_battery.lua
@@ -1,7 +1,6 @@
 -- If the laptop is on battery, the VO set in the config will be choosen,
 -- else the one defined with „hqvo“ is used.
 local hqvo = "opengl-hq:scale-antiring=0.7:icc-profile-auto:blend-subtitles=yes"
-local vsync = "display-resample-vdrop"
 local utils = require 'mp.utils'
 if mp.get_property_bool("option-info/vo/set-from-commandline") == true then
     return
@@ -11,5 +10,4 @@ t.args = {"/usr/bin/pmset", "-g", "ac"}
 res = utils.subprocess(t)
 if res.stdout ~= "No adapter attached.\n" then
     mp.set_property("options/vo", hqvo)
-    mp.set_property("options/video-sync", vsync)
 end
