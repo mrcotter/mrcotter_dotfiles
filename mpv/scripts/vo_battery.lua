8f6f4e054ae7a268a4856c5996742caa33ac8da1
Kris Zheng <kris@192-168-1-100.tpgi.com.au>
2016-10-03 01:25:34 +1100

    Change conf settings for Sierra and new MPV version

--- mpv/scripts/vo_battery.lua
+++ mpv/scripts/vo_battery.lua
@@ -1,6 +1,6 @@
 -- If the laptop is on battery, the VO set in the config will be choosen,
 -- else the one defined with „hqvo“ is used.
-local hqvo = "opengl-hq:icc-profile-auto:3dlut-size=256x256x256:blend-subtitles=yes"
+local hqvo = "opengl-hq:icc-profile-auto:blend-subtitles=yes"
 local utils = require 'mp.utils'
 if mp.get_property_bool("option-info/vo/set-from-commandline") == true then
     return
